206bc12ea3f5291797e4aa845280c4915f8f739d
Unknown <zlchldjyy@gmail.com>
2020-02-17 16:19:45 +0800

    Docker Jenkins for Infinity same name branch

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM postgres:12.1
+
+ENV POSTGRES_USER=kensin  
+ENV POSTGRES_PASSWORD=989277229  
+# database name
+ENV POSTGRES_DB=DEV_DB  
---
+++ pssql.sh
@@ -0,0 +1,5 @@
+# \c <database name>
+# \d or \dt view the tables of defined databse
+# \q exit the container
+# \d tablename describe the info of table
+docker exec -it postgres_db psql -U kensin -d DEV_DB
